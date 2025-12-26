3ff0fbcaea6d51ab470eed51e50206fe3f0c27d2
Sujatha-Sankar <sujasankar1994.com>
2025-12-26 12:13:51 +0530

    Fix pytest import issue in CI

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,6 @@
-import sys
-import os
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
 from app.main import app
+
+def test_health():
+    client = app.test_client()
+    response = client.get("/")
+    assert response.status_code == 200
