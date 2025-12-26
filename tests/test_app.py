2d5f77b8d96d95ccdb3d6b9e60a0521707d60d39
Sujatha-Sankar <sujasankar1994.com>
2025-12-26 09:28:16 +0530

    Fix pytest path issue in CI

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,11 +1,6 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from app.main import app
-
-def test_health():
-    client = app.test_client()
-    response = client.get("/")
-    assert response.status_code == 200
-
-def test_hello():
-    client = app.test_client()
-    response = client.get("/hello")
-    assert response.status_code == 200
